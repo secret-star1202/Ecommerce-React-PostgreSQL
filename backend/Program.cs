f4521fde4c7352472bcc669c1c353406ac0aa4ad
secret-star1202 <starDev1202@outlook.com>
2023-04-11 01:49:56 +0300

    check swagger config

--- backend/Program.cs
+++ backend/Program.cs
@@ -53,32 +53,30 @@ builder.Services.AddDbContext<DataContext>(options => options.UseNpgsql(builder.
 
 var app = builder.Build();
 
-//Configure the HTTP request pipeline.
+//     app.UseSwagger();
+//     app.UseSwaggerUI();
+
+// // This middleware serves the Swagger documentation UI
+//     app.UseSwaggerUI(c =>
+//     {
+//         c.SwaggerEndpoint("/swagger/v1/swagger.json", "E-commerce API V1");
+//         c.RoutePrefix = string.Empty;
+//     });
+
+
 app.UseSwagger();
 if (app.Environment.IsDevelopment())
 {
-   app.UseSwaggerUI(c =>
+    app.UseSwaggerUI();
+}
+if (!app.Environment.IsDevelopment())
 {
-    c.SwaggerEndpoint("swagger/v1/swagger.json", "E-commerce API v1");
-    c.RoutePrefix = string.Empty;
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        c.RoutePrefix = string.Empty;
+    });
 }
-);
-}
-
-
-// app.UseSwagger();
-// if (app.Environment.IsDevelopment())
-// {
-//     app.UseSwaggerUI();
-// }
-// if (!app.Environment.IsDevelopment())
-// {
-//     app.UseSwaggerUI(options =>
-//     {
-//         options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
-//         options.RoutePrefix = string.Empty;
-//     });
-// }
  
 
 app.UseAuthentication();
