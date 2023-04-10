3d4391886dd4658a8a591e50b183aa8e25ee5586
secret-star1202 <starDev1202@outlook.com>
2023-04-10 23:14:40 +0300

    refactor programcs, configure swaggerui

--- backend/Program.cs
+++ backend/Program.cs
@@ -53,32 +53,33 @@ builder.Services.AddDbContext<DataContext>(options => options.UseNpgsql(builder.
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-// if (app.Environment.IsDevelopment())
-// {
-//     app.UseSwagger();
-//     app.UseSwaggerUI();
-// }
-
-// app.UseSwaggerUI(c =>
-// {
-//     c.SwaggerEndpoint("swagger/v1/swagger.json", "E-commerce API v1");
-//     c.RoutePrefix = string.Empty;
-// }
-// );
+//Configure the HTTP request pipeline.
 app.UseSwagger();
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwaggerUI();
-}
-if (!app.Environment.IsDevelopment())
+   app.UseSwaggerUI(c =>
 {
-    app.UseSwaggerUI(options =>
-    {
-        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
-        options.RoutePrefix = string.Empty;
-    });
+    c.SwaggerEndpoint("swagger/v1/swagger.json", "E-commerce API v1");
+    c.RoutePrefix = string.Empty;
 }
+);
+}
+
+
+// app.UseSwagger();
+// if (app.Environment.IsDevelopment())
+// {
+//     app.UseSwaggerUI();
+// }
+// if (!app.Environment.IsDevelopment())
+// {
+//     app.UseSwaggerUI(options =>
+//     {
+//         options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+//         options.RoutePrefix = string.Empty;
+//     });
+// }
+ 
 
 app.UseAuthentication();
 app.UseHttpsRedirection();
