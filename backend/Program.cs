d723c6ab9ff7ceacb4f3a49d6b1efdcb8a19689b
secret-star1202 <starDev1202@outlook.com>
2023-04-10 12:48:43 +0300

    configure swagger main endpoint

--- backend/Program.cs
+++ backend/Program.cs
@@ -57,7 +57,12 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+  app.UseSwaggerUI(c =>
+ {
+   c.SwaggerEndpoint("swagger/v1/swagger.json", "E-commerce API v1");
+   c.RoutePrefix = string.Empty;
+ }
+  );
 }
 
 app.UseAuthentication();
