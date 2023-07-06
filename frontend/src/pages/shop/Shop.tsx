07a5f81098cbca3a87e389b5dfb4ed0b34985af7
secret-star1202 <starDev1202@outlook.com>
2023-07-07 02:11:56 +0300

    refactor shop component, reusable product card

--- frontend/src/pages/shop/Shop.tsx
+++ frontend/src/pages/shop/Shop.tsx
@@ -1,27 +1,19 @@
 import { ChangeEvent, useEffect, useState } from 'react';
-import { Box, CardMedia, Pagination, Typography } from '@mui/material';
+import { Box, Pagination } from '@mui/material';
 import {
   PageContainer,
   CardsWrapper,
   PaginationContainer,
-  ProdCard,
-  ProductCardButton,
-  ProductCardContent,
-  ProductCardName,
-  ProductCardPrice,
-  CardImageContainer,
 } from './Shop.styles';
 import { useAppSelector, useAppDispatch } from '../../hooks/reduxHook';
 import { fetchAllProducts } from '../../redux/reducers/productSlice';
 import DropdownOption from '../../components/dropdown-option/DropdownOption';
-import { addToCart } from '../../redux/reducers/cartSlice';
 import CategoryLists from '../../components/categories/CategoryLists';
-import { useNavigate } from 'react-router-dom';
+import ProductCard from '../../components/product-card/ProductCard';
 
 const Shop = () => {
   const products = useAppSelector((state) => state.productReducer);
   const dispatch = useAppDispatch();
-  const navigate = useNavigate();
   const [currentPage, setCurrentPage] = useState(1);
   const itemsPerPage = 12;
 
@@ -64,52 +56,7 @@ const Shop = () => {
       <CardsWrapper>
         {currentProducts.length > 0 &&
           currentProducts.map((product) => (
-            <ProdCard key={product.id}>
-              <CardImageContainer
-                onClick={() => navigate(`/category/${product.name}`)}
-              >
-                <CardMedia component="img" height="200" image={product.image} />
-                <Box
-                  sx={{
-                    position: 'absolute',
-                    color: 'white',
-                    top: 3,
-                    padding: '2px 5px',
-                    background: '#32CD32',
-                    display: 'flex',
-                    justifyContent: 'center',
-                    borderRadius: '50px',
-                  }}
-                >
-                  <Typography
-                    sx={{
-                      fontSize: '10px',
-                      textTransform: 'uppercase',
-                    }}
-                  >
-                    {product.categoryName}
-                  </Typography>
-                </Box>
-              </CardImageContainer>
-
-              <ProductCardContent>
-                <ProductCardButton
-                  variant="outlined"
-                  color="inherit"
-                  onClick={() => dispatch(addToCart(product))}
-                >
-                  ADD TO CART
-                </ProductCardButton>
-                <ProductCardName
-                  sx={{
-                    textTransform: 'uppercase',
-                  }}
-                >
-                  {product.name}
-                </ProductCardName>
-                <ProductCardPrice>$ {product.price}</ProductCardPrice>
-              </ProductCardContent>
-            </ProdCard>
+            <ProductCard key={product.id} product={product} />
           ))}
       </CardsWrapper>
       <PaginationContainer>
